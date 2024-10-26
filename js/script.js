777dfd1e46530cfd6690fa7778090c1029bba9a5
“Your <andrijlosk30@gmail.com>
2024-10-26 13:56:27 +0200

    fixing

--- js/script.js
+++ js/script.js
@@ -23,67 +23,63 @@
 
 //task2
 
-// const users = [
-//   { name: "Mango", active: true },
-//   { name: "Poly", active: false },
-//   { name: "Ajax", active: true },
-//   { name: "Lux", active: false },
-// ];
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
 
-// const toggleUserState = (allUsers, userName) => {
-//   return new Promise((resolve, reject) => {
-//     allUsers.map((user) =>
-//       user.name === userName ? { ...user, active: !user.active } : user
-//     );
-
-//     resolve(
-//       allUsers.map((user) =>
-//         user.name === userName ? { ...user, active: !user.active } : user
-//       )
-//     );
-
-//     reject("error");
-//   });
-// };
-
-// const logger = (updatedUsers) => console.table(updatedUsers);
-
-// toggleUserState(users, "Mango").then(logger);
-// toggleUserState(users, "Lux").then(logger);
-
-//task3
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const makeTransaction = (transaction) => {
+const toggleUserState = (allUsers, userName) => {
   return new Promise((resolve, reject) => {
-    const delay = randomIntegerFromInterval(200, 500);
-    setTimeout(() => {
-      const canProcess = Math.random() > 0.3;
+    resolve(
+      allUsers.map((user) =>
+        user.name === userName ? { ...user, active: !user.active } : user
+      )
+    );
 
-      if (canProcess) {
-        resolve([transaction.id, delay]);
-      } else {
-        reject(transaction.id);
-      }
-    }, delay);
+    reject("error");
   });
 };
 
-const logSuccess = ([id, time]) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
-};
+const logger = (updatedUsers) => console.table(updatedUsers);
 
-const logError = (id) => {
-  console.warn(`Error processing transaction ${id}. Please try again later.`);
-};
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
 
-makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+//task3
 
-makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+// const randomIntegerFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
 
-makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+// const makeTransaction = (transaction) => {
+//   return new Promise((resolve, reject) => {
+//     const delay = randomIntegerFromInterval(200, 500);
+//     setTimeout(() => {
+//       const canProcess = Math.random() > 0.3;
 
-makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+//       if (canProcess) {
+//         resolve([transaction.id, delay]);
+//       } else {
+//         reject(transaction.id);
+//       }
+//     }, delay);
+//   });
+// };
+
+// const logSuccess = ([id, time]) => {
+//   console.log(`Transaction ${id} processed in ${time}ms`);
+// };
+
+// const logError = (id) => {
+//   console.warn(`Error processing transaction ${id}. Please try again later.`);
+// };
+
+// makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+// makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+// makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+// makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
